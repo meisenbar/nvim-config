cff54baf3384c43cb5b1c61dd0cba4cbfdd8db50
Marc Eisenbarth <marc.eisenbarth@ptwdosimetry.com>
2025-09-22 15:48:56 +0200

    follow documentation of vue_ls for vtsls setup

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,5 +1,5 @@
 vim.lsp.config('lua_ls', {
-  settings = {
+	settings = {
 		Lua = {
 			runtime = {
 				version = 'LuaJIT'
@@ -9,5 +9,36 @@ vim.lsp.config('lua_ls', {
 				globals = { "vim" }
 			}
 		}
-  },
+	},
 })
+
+local vue_language_server_path = vim.fn.expand '$MASON/packages' ..
+		'/vue-language-server' .. '/node_modules/@vue/language-server'
+
+local tsserver_filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' }
+
+local vue_plugin = {
+	name = '@vue/typescript-plugin',
+	location = vue_language_server_path,
+	languages = { 'vue' },
+	configNamespace = 'typescript',
+}
+
+local vtsls_config = {
+	settings = {
+		vtsls = {
+			tsserver = {
+				globalPlugins = {
+					vue_plugin,
+				},
+			},
+		},
+	},
+	filetypes = tsserver_filetypes,
+}
+
+local vue_ls_config = {}
+
+vim.lsp.config('vtsls', vtsls_config)
+vim.lsp.config('vue_ls', vue_ls_config)
+vim.lsp.enable({ 'vtsls', 'vue_ls' })
