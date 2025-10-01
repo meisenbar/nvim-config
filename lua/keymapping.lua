fe67cb111121eeb864e9561f6addc16f518fa4f5
Marc Eisenbarth <marc.eisenbarth@ptwdosimetry.com>
2025-10-01 09:57:28 +0200

    add nvim-dap

--- lua/keymapping.lua
+++ lua/keymapping.lua
@@ -9,11 +9,13 @@ vim.keymap.set('n', '<C-J>', '<C-W><C-J>', { noremap = true, silent = true })
 vim.keymap.set('n', '<C-K>', '<C-W><C-K>', { noremap = true, silent = true })
 vim.keymap.set('n', '<C-L>', '<C-W><C-L>', { noremap = true, silent = true })
 
+-- Page Navigation
+vim.keymap.set('n', '<Space-J>', '<C-U>', { noremap = true, silent = true })
+vim.keymap.set('n', '<Space-K>', '<C-D>', { noremap = true, silent = true })
 
 -- Diagnostics
 vim.keymap.set('n', '[d', vim.diagnostic.goto_next, { noremap = true, silent = true })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_prev, { noremap = true, silent = true })
-
 
 -- LSP
 vim.keymap.set('n', '<Leader>a', vim.lsp.buf.code_action, { noremap = true, silent = true })
---
+++ lua/plugins/nvim-dap.lua
@@ -0,0 +1,5 @@
+return {
+	"mfussenegger/nvim-dap",
+	"jay-babu/mason-nvim-dap.nvim",
+	ensure_installed = { 'js-debug-adapter' },
+}
