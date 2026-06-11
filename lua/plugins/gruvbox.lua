d2b438c547f8f68eacfb289e694bc4186ca0443f
Marc Eisenbarth <marc.eisenbarth@ptwdosimetry.com>
2026-06-11 10:50:37 +0200

    update packages and add omnisharp

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -1,6 +1,9 @@
 -- https://github.com/ellisonleao/gruvbox.nvim
 return {
-  "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = {
-		terminal_colors = false
+	"ellisonleao/gruvbox.nvim",
+	priority = 1000,
+	config = true,
+	opts = {
+		terminal_colors = true,
 	}
 }
--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -8,12 +8,20 @@ return {
 		"nvim-lua/plenary.nvim",
 		"antoinemadec/FixCursorHold.nvim",
 		"marilari88/neotest-vitest",
+		"thenbe/neotest-playwright",
+		dependencies = 'nvim-telescope/telescope.nvim',
 	},
 
 	config = function()
 		require("neotest").setup({
 			adapters = {
 				require("neotest-vitest"),
+				require('neotest-playwright').adapter({
+					options = {
+						persist_project_selection = true,
+						enable_dynamic_test_discovery = true,
+					},
+				}),
 			},
 			highlights = {
 				adapter_name = "Title",
